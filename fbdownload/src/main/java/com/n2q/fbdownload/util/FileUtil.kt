0177cfe727038ba58ec1428044e7d446c505c4b5
n2q <quan.n2q.bn@gmail.com>
2022-01-12 15:51:41 +0700

    [RELEASE] - 2.0.1: Fix generate file name + external name

--- fbdownload/src/main/java/com/n2q/fbdownload/util/FileUtil.kt
+++ fbdownload/src/main/java/com/n2q/fbdownload/util/FileUtil.kt
@@ -1,24 +1,50 @@
 package com.n2q.fbdownload.util
 
 import android.content.Context
+import com.n2q.fbdownload.constant.Constant
+import com.n2q.fbdownload.entities.TypeMedia
 import java.io.File
 
 object FileUtil {
 
-    const val EXTERNAL_NAME = "FDownload"
+    fun externalFileChild(context: Context, externalName: String, nameChild: String) = File(externalFileDir(context, externalName).toString(), nameChild)
 
-    fun externalFileChild(context: Context, nameChild: String) = File(externalPathDir(context), nameChild)
+    fun externalFileDir(context: Context, externalName: String) = File(context.getExternalFilesDir(null), externalName)
 
-    fun externalFileDir(context: Context) = File(context.getExternalFilesDir(null), EXTERNAL_NAME)
-
-    fun externalPathDir(context: Context) = externalFileDir(context).toString()
-
-    fun isExistExternalDir(context: Context): Boolean {
-        val dir = externalFileDir(context)
+    fun isExistExternalDir(context: Context, externalName: String): Boolean {
+        val dir = externalFileDir(context, externalName)
         if (!dir.exists()) {
             return dir.mkdirs()
         }
         return true
+    }
+
+    private fun generateFilename(context: Context, externalName: String, filename: String, typeMedia: TypeMedia): String {
+        var name = String()
+        var ext = String()
+
+        if (typeMedia == TypeMedia.SD_VIDEO) {
+            name = "${Constant.PREFIX_SD}$filename"
+            ext = Constant.EXT_VIDEO
+        }
+
+        if (typeMedia == TypeMedia.HD_VIDEO) {
+            name = "${Constant.PREFIX_HD}$filename"
+            ext = Constant.EXT_VIDEO
+        }
+
+        if (typeMedia == TypeMedia.AUDIO) {
+            name = "${Constant.PREFIX_AUDIO}$filename"
+            ext = Constant.EXT_AUDIO
+        }
+
+        var count = 1
+        var file = externalFileChild(context, externalName, name + ext)
+        while (file.exists()) {
+            file = externalFileChild(context, externalName, "$name-$count$ext")
+            count++
+        }
+        return file.name
     }
 
 
